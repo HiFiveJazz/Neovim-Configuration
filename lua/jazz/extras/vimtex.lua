b85e900e4579a7b91f7f09ed15ae3a14c99c314b
Jasmeet (Jazz) Bhatia <146850250+HiFiveJazz@users.noreply.github.com>
2024-04-16 22:05:29 +0000

    Update vimtex.lua

--- lua/jazz/extras/vimtex.lua
+++ lua/jazz/extras/vimtex.lua
@@ -4,8 +4,9 @@ local M = {
 }
 
 function M.config()
-  vim.cmd('syntax enable')
+  vim.g.tex_flavor='latex'
   vim.g.vimtex_view_method = 'zathura'
+  vim.g.vimtex_quickfix_mode = 0
 end
 
 return M
