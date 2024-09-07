710d0b4525a698e30e3ec9cb564b421867e118b0
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-09-06 21:47:27 -0700

    debug updates

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -3,14 +3,117 @@ local M = {
   event = "VeryLazy",
   dependencies = {
     {
+      -- "mfussenegger/nvim-dap-python",
       "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
       "theHamsta/nvim-dap-virtual-text",
       "nvim-telescope/telescope-dap.nvim",
+      "nvim-neotest/nvim-nio"
     },
   },
 }
 function M.config()
+
+  local dap = require("dap")
+  local ui = require("dapui")
+  require("dapui").setup()
+
+  -- Python
+  dap.adapters.python = function(cb, config)
+    if config.request == 'attach' then
+      ---@diagnostic disable-next-line: undefined-field
+      local port = (config.connect or config).port
+      ---@diagnostic disable-next-line: undefined-field
+      local host = (config.connect or config).host or '127.0.0.1'
+      cb({
+        type = 'server',
+        port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+        host = host,
+        options = {
+          source_filetype = 'python',
+        },
+      })
+    else
+      cb({
+        type = 'executable',
+        command = '/home/jazz/.local/share/nvim/mason/packages/debugpy/venv/bin/python',
+        -- command = 'path/to/virtualenvs/debugpy/bin/python',
+        args = { '-m', 'debugpy.adapter' },
+        options = {
+          source_filetype = 'python',
+        },
+      })
+    end
+  end
+
+  dap.configurations.python = {
+    {
+      -- The first three options are required by nvim-dap
+      type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+      request = 'launch';
+      name = "Launch file";
+
+      -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+      program = "${file}"; -- This configuration will launch the current file if used.
+      pythonPath = function()
+        -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+        -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+        -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+        local cwd = vim.fn.getcwd()
+        if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+          return cwd .. '/venv/bin/python'
+        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+          return cwd .. '/.venv/bin/python'
+        else
+          return '/usr/bin/python'
+        end
+      end;
+    },
+  }
+
+  -- C language
+  dap.adapters.gdb = {
+    type = "executable",
+    command = "gdb",
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+  }
+
+  dap.configurations.c = {
+    {
+      name = "Launch",
+      type = "gdb",
+      request = "launch",
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = "${workspaceFolder}",
+      stopAtBeginningOfMainSubprogram = false,
+    },
+    {
+      name = "Select and attach to process",
+      type = "gdb",
+      request = "attach",
+      program = function()
+         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      pid = function()
+         local name = vim.fn.input('Executable name (filter): ')
+         return require("dap.utils").pick_process({ filter = name })
+      end,
+      cwd = '${workspaceFolder}'
+    },
+    {
+      name = 'Attach to gdbserver :1234',
+      type = 'gdb',
+      request = 'attach',
+      target = 'localhost:1234',
+      program = function()
+         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}'
+    },
+  }
+
   local wk = require "which-key"
   wk.add {
     { "<leader>dC", "<cmd>lua require'dap'.run_to_cursor()<cr>", desc = "Run To Cursor" },
---
+++ test.py
@@ -0,0 +1,3 @@
+print("Hello")
+print("troll")
+print("bye")
