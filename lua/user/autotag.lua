5c4675a1d8ab16c60591dd241c947555142d60b0
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2025-05-27 03:06:40 -0700

    update autotag to be up to spec!

--- lua/user/autotag.lua
+++ lua/user/autotag.lua
@@ -1,35 +1,38 @@
 local M = {
   "windwp/nvim-ts-autotag",
+  event = { "BufReadPre", "BufNewFile" },
 }
 
 function M.config()
-  require("nvim-ts-autotag").setup {
-    enable = true,
-    enable_rename = true,
-    enable_close = false,
-    enable_close_on_slash = false,
-
-    filetypes = {
-      "html",
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-      "svelte",
-      "vue",
-      "tsx",
-      "jsx",
-      "rescript",
-      "xml",
-      "php",
-      "markdown",
-      "astro",
-      "glimmer",
-      "handlebars",
-      "hbs",
+  require("nvim-ts-autotag").setup({
+    opts = {
+      enable_close = false,
+      enable_rename = true,
+      enable_close_on_slash = false,
     },
-  }
-
+    -- Use per_filetype to customize behavior per language
+    per_filetype = {
+      html = {
+        enable_close = false,
+      },
+      javascript = {},
+      typescript = {},
+      javascriptreact = {},
+      typescriptreact = {},
+      svelte = {},
+      vue = {},
+      tsx = {},
+      jsx = {},
+      rescript = {},
+      xml = {},
+      php = {},
+      markdown = {},
+      astro = {},
+      glimmer = {},
+      handlebars = {},
+      hbs = {},
+    },
+  })
 end
 
 return M
